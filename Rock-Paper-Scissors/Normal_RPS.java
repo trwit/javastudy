a0967f8b7bc84759cf4de59209d95c5878576b18
trice <160309591+trwit@users.noreply.github.com>
2024-03-13 19:00:49 +0900

    Update Normal_RPS.java

--- Rock-Paper-Scissors/Normal_RPS.java
+++ Rock-Paper-Scissors/Normal_RPS.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 /**
  * Class Name   : Normal_RPS
- * Author       : 202-09
+ * Author       : trwit
  * Created Date : 2024. 2. 23.
- * Version      : 1.0
+ * Version      : 1.0.1
  * Purpose      :
  * Desciption   :
  */
@@ -19,10 +19,7 @@ public class Normal_RPS {
 	public static void main(String[] args) {
 		System.out.println("Let's play Rock, Paper, Scissors.");
 		System.out.println("Choose one.");
-		System.out.println("1. Rock, 2. Scissors, 3. Paper");
-		// You may have a question.
-		// "Why Scissors are NUMBER TWO."
-		// "Because My coding skills are not good as modify."
+		System.out.println("1. Rock, 2. Paper, 3. Scissors");
 		Scanner scan = new Scanner(System.in);
 		System.out.print(">>> ");
 		int Int = Integer.parseInt(scan.nextLine());
@@ -32,36 +29,35 @@ public class Normal_RPS {
 		if (Int==1) {
 			RPS = "rock";
 		}else if(Int==2){
-			RPS = "scissors";
-		}else {
 			RPS = "paper";
+		}else {
+			RPS = "scissors";
 		}
 		System.out.println("You: " + RPS);
 		
 		String ComRPS = "";
 		int comRandInt = (int)(Math.random()*3)+1;
-//		System.out.println(comrandInt);
 		if (comRandInt==1) {
 			ComRPS = "rock";
 		}else if(comRandInt==2){
-			ComRPS = "scissors";
-		}else {
 			ComRPS = "paper";
+		}else {
+			ComRPS = "scissors";
 		}
 		System.out.println("Com: " + ComRPS);
 		
 		if (Int==1&&comRandInt==3) {
-			System.out.println("Lose");
+			System.out.println("Win");
 		}else if(Int==2&&comRandInt==1) {
-			System.out.println("Lose");			
-		}else if(Int==3&&comRandInt==2) {
-			System.out.println("Lose");						
-		}else if(Int==1&&comRandInt==2) {
-			System.out.println("Win");
-		}else if(Int==2&&comRandInt==3) {
 			System.out.println("Win");			
+		}else if(Int==3&&comRandInt==2) {
+			System.out.println("Win");						
+		}else if(Int==1&&comRandInt==2) {
+			System.out.println("Lose");
+		}else if(Int==2&&comRandInt==3) {
+			System.out.println("Lose");			
 		}else if(Int==3&&comRandInt==1) {
-			System.out.println("Win");
+			System.out.println("Lose");
 		}else if(Int==1&&comRandInt==1){
 			System.out.println("Drew");
 		}else if(Int==2&&comRandInt==2){
