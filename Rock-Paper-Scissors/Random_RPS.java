ec5e077b8ed9aa010e9d46af4cffa8ccd0c73a79
trice <160309591+trwit@users.noreply.github.com>
2024-03-13 18:57:29 +0900

    Update Random_RPS.java

--- Rock-Paper-Scissors/Random_RPS.java
+++ Rock-Paper-Scissors/Random_RPS.java
@@ -2,9 +2,9 @@ package sample;
 
 /**
  * Class Name   : Random_RPS
- * Author       : 202-09
+ * Author       : trwit
  * Created Date : 2024. 2. 21.
- * Version      : 1.0
+ * Version      : 1.0.1
  * Purpose      :
  * Desciption   :
  */
@@ -17,43 +17,42 @@ public class Random_RPS {
 		System.out.println("Let's play Rock, Paper, Scissors.");
 		System.out.println("But this one can't be control anything.");
 		System.out.print("\n");
+		// In this case, Nobody may focus on the number..
 		
 		String RPS = "";
 		int randInt = (int)(Math.random()*3)+1;
-//		System.out.println(randInt);
 		if (randInt==1) {
 			RPS = "rock";
 		}else if(randInt==2){
-			RPS = "scissors";
-		}else {
 			RPS = "paper";
+		}else {
+			RPS = "scissors";
 		}
 		System.out.println("You: " + RPS);
 		
 		String ComRPS = "";
 		int comRandInt = (int)(Math.random()*3)+1;
-//		System.out.println(comrandInt);
 		if (comRandInt==1) {
 			ComRPS = "rock";
 		}else if(comRandInt==2){
-			ComRPS = "scissors";
-		}else {
 			ComRPS = "paper";
+		}else {
+			ComRPS = "scissors";
 		}
 		System.out.println("Com: " + ComRPS);
 		
 		if (randInt==1&&comRandInt==3) {
-			System.out.println("Lose");
+			System.out.println("Win");
 		}else if(randInt==2&&comRandInt==1) {
-			System.out.println("Lose");			
-		}else if(randInt==3&&comRandInt==2) {
-			System.out.println("Lose");						
-		}else if(randInt==1&&comRandInt==2) {
-			System.out.println("Win");
-		}else if(randInt==2&&comRandInt==3) {
 			System.out.println("Win");			
+		}else if(randInt==3&&comRandInt==2) {
+			System.out.println("Win");						
+		}else if(randInt==1&&comRandInt==2) {
+			System.out.println("Lose");
+		}else if(randInt==2&&comRandInt==3) {
+			System.out.println("Lose");			
 		}else if(randInt==3&&comRandInt==1) {
-			System.out.println("Win");
+			System.out.println("Lose");
 		}else if(randInt==1&&comRandInt==1){
 			System.out.println("Drew");
 		}else if(randInt==2&&comRandInt==2){
